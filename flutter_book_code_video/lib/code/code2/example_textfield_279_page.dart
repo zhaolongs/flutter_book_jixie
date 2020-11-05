0c314ee921a3ddb599ea4831f1a1ba7aff8f4bfb
zhaolong <928343994@qq.com>
2020-11-05 08:28:21 +0800

    文本输入框 TextField 的基本使用 常用的一些实际应用效果

--- flutter_book_code_video/lib/code/code2/example_textfield_279_page.dart
+++ flutter_book_code_video/lib/code/code2/example_textfield_279_page.dart
@@ -14,6 +14,7 @@ import 'package:flutter/material.dart';
 ///lib/code/code2/example_scaffold_216_page.dart
 ///Scaffold的基本使用 内容主体页面
 import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
 import 'package:flutterbookcode/demo/rich_text_tag.dart';
 
 //应用入口
@@ -44,7 +45,8 @@ class _ExampleState extends State<Example279> {
         child: Column(children: [
           // buildTextField(),
           // buildTextField2(),
-          buildTextField3(),
+          //buildTextField3(),
+          buildTextField8(),
         ]),
       ),
     );
@@ -73,6 +75,67 @@ class _ExampleState extends State<Example279> {
     return new TextField(
       //输入的字数最多为 11字
       maxLength: 11,
+    );
+  }
+
+  // 字数输入限制
+  // 实现方式二   右下角不会有文字计数器
+  Widget buildTextField4() {
+    return new TextField(
+      //输入文本格式过滤
+      inputFormatters: <TextInputFormatter>[
+        //当长度大于11的时候就不允许再输入了
+        LengthLimitingTextInputFormatter(11)
+      ],
+    );
+  }
+
+  // 限制只能输入数字
+  Widget buildTextField5() {
+    return new TextField(
+      //设置键盘的类型
+      keyboardType: TextInputType.phone,
+      //输入文本格式过滤
+      inputFormatters: [
+        //输入的内容长度为11位
+        LengthLimitingTextInputFormatter(11),
+        //只允许输入数字
+        WhitelistingTextInputFormatter.digitsOnly
+      ],
+    );
+  }
+
+  // 限制只能输入字母 正则过滤
+  Widget buildTextField6() {
+    return new TextField(
+      //输入文本格式过滤
+      inputFormatters: [
+        //使用正则过滤
+        WhitelistingTextInputFormatter(RegExp("[a-zA-Z]")),
+      ],
+    );
+  }
+
+  //限制单行
+  Widget buildTextField7() {
+    return new TextField(
+      //设置最大行数为3行
+      maxLines: 3,
+      //输入文本格式过滤
+      inputFormatters: [
+        //限制单行
+        BlacklistingTextInputFormatter.singleLineFormatter
+      ],
+    );
+  }
+
+  //输入邮箱控制
+  Widget buildTextField8() {
+    return new TextField(
+      //输入文本格式过滤
+      inputFormatters: [
+        WhitelistingTextInputFormatter(RegExp("[@]|[A-Za-z0-9]|[.]")),
+      ],
     );
   }
 }
