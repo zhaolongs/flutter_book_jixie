1680e4f58f6e60e90398ad91082644eb8f5f940b
zhaolong <928343994@qq.com>
2020-11-11 08:58:59 +0800

    RadioListTile 添加

--- flutter_book_code_video/lib/code/code4/example_404_radio_page.dart
+++ flutter_book_code_video/lib/code/code4/example_404_radio_page.dart
@@ -20,7 +20,6 @@ void main() {
   ));
 }
 
-
 class Example404 extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -35,7 +34,13 @@ class _ExampleState extends State<Example404> {
       appBar: AppBar(
         title: Text("单选框Radio"),
       ),
-      body: buildRadioGroupWidget(),
+
+      body: Column(children: [
+        SizedBox(height: 26,),
+        buildRadioListTile(),
+        SizedBox(height: 66,),
+        buildRadioGroupWidget(),
+      ],),
     );
   }
 
@@ -72,5 +77,46 @@ class _ExampleState extends State<Example404> {
         },
       ),
     ]);
+  }
+  ///代码清单 4-7  单选框RadioListTile组件的基本使用
+  ///lib/code/code4/example_404_radio_page.dart
+  Widget buildRadioListTile() {
+    return RadioListTile(
+      // 当前对应的单选框的标识
+      value: 0,
+      //是否选中发生变化时的回调， 回调的 bool 值 就是是否选中 , true 是 选中
+      onChanged: (value) {
+        setState(() {
+          groupValue = value;
+        });
+      },
+      // 选中时 Radio 的填充的颜色 ，
+      activeColor: Colors.red,
+      // 标题， selected 如果是 true
+      // 如果 不设置 text 的 color 的话，
+      // text的颜色使用activeColor
+      title: Text(
+        "标题",
+      ),
+      // 副标题（在 title 下面）
+      subtitle: Text("副标题"),
+      //是否是三行文本
+      //如果是 true 副标题 不能为 null
+      //如果是 false 如果没有副标题
+      // 就只有一行， 如果有副标题 ，就只有两行
+      isThreeLine: true,
+      // 是否密集垂直
+      dense: false,
+      // 左边的控件
+      secondary: Image.asset(
+        "assets/images/2.0x/logo.jpg",
+        fit: BoxFit.fill,
+      ),
+      // text 和 icon 的 color 是否 是 activeColor 的颜色
+      selected: false,
+      //方向模型
+      controlAffinity: ListTileControlAffinity.trailing,
+      groupValue: groupValue,
+    );
   }
 }
