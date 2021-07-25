7bf980a4633cdf4c46e5ee4de1139a9cf8d0a057
zhaolong <928343994@qq.com>
2021-07-26 07:55:46 +0800

    代码清单 6-13 [BorderRadiusTween] 的基本使用

--- flutter_book_code_video/lib/code/code6/example_615_BorderRadiusTween.dart
+++ flutter_book_code_video/lib/code/code6/example_615_BorderRadiusTween.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -24,7 +23,7 @@ void main() {
   ));
 }
 
-/// 代码清单 6-14 [BorderRadiusTween] 的基本使用
+/// 代码清单 6-13 [BorderRadiusTween] 的基本使用
 ///lib/code/code6/example_615_BorderRadiusTween.dart
 class Example615 extends StatefulWidget {
   @override
