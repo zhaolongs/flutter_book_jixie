e8438957e054b1d246dcfdb12f749a8817c6754f
zhaolong <928343994@qq.com>
2021-07-26 07:57:27 +0800

    代码清单 6-15 [TextStyleTween] 文本样式过渡动画的基本使用

--- flutter_book_code_video/lib/code/code6/example_617_TextStyleTween.dart
+++ flutter_book_code_video/lib/code/code6/example_617_TextStyleTween.dart
@@ -23,7 +23,7 @@ void main() {
   ));
 }
 
-/// 代码清单 6-16 [TextStyleTween] 文本样式过渡动画的基本使用
+/// 代码清单 6-15 [TextStyleTween] 文本样式过渡动画的基本使用
 ///lib/code/code6/example_617_TextStyleTween.dart
 class Example617 extends StatefulWidget {
   @override
